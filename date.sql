f8c7c454da7be26d3648941ea4f1bbee8e6fcefb
Tamim Iqbal Maruf <marufmd775@gmail.com>
2024-03-17 11:28:48 +0600

    Date Functions In PostgreSQL

--- date.sql
+++ date.sql
@@ -23,3 +23,15 @@ SELECT now():: date;
 SELECT now():: time;
 
 SELECT to_char(now(), 'dd/mm/yyyy');
+
+SELECT to_char(now(), 'ddd');
+
+SELECT CURRENT_DATE - INTERVAL '1 Year';
+
+SELECT age(CURRENT_DATE, '2001-04-27');
+
+SELECT *, age(CURRENT_DATE, dob) from students;
+
+SELECT extract(year from '2024-01-25':: date);
+
+SELECT 0:: BOOLEAN;
