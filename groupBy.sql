32cf988e9690a1181b07bd874915e658effa0d8f
Tamim Iqbal Maruf <marufmd775@gmail.com>
2024-03-17 12:08:23 +0600

    Grouping And Filtering Data With GROUP BY And HAVING

---
+++ groupBy.sql
@@ -0,0 +1,11 @@
+-- Active: 1709621644738@@127.0.0.1@5432@ph
+
+SELECT * from students;
+
+SELECT age, count(*) from students GROUP BY age;
+
+SELECT age, count(*) from students GROUP BY age HAVING avg(age) > 20;
+
+SELECT extract(year from dob) as birth_year, count(*) from students
+    GROUP BY birth_year
+
