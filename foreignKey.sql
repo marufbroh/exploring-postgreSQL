64d7165f1a699d00b509150460aabf091a09b153
Tamim Iqbal Maruf <marufmd775@gmail.com>
2024-03-18 23:41:12 +0600

    user and post table data inserted

--- foreignKey.sql
+++ foreignKey.sql
@@ -1,12 +1,53 @@
 -- Active: 1709621644738@@127.0.0.1@5432@ph
 
 CREATE TABLE "user" (
-    id SERIAL PRIMARY KEY,
-    username VARCHAR(25) not NULL
+    id SERIAL PRIMARY KEY, username VARCHAR(25) not NULL
 );
 
 CREATE TABLE post (
-    id SERIAL PRIMARY KEY,
-    title TEXT NOT NULL,
-    user_id INTEGER REFERENCES "user"(id)
-)
+    id SERIAL PRIMARY KEY, title TEXT NOT NULL, user_id INTEGER REFERENCES "user" (id)
+);
+
+INSERT INTO
+    "user" (username)
+VALUES ('akash'),
+    ('batash'),
+    ('sagor'),
+    ('nodi');
+
+SELECT * from "user";
+
+INSERT INTO
+    post (title, user_id)
+VALUES (
+        'Exploring the World of Machine Learning', 1
+    ),
+    (
+        'The Art of Cooking: A Culinary Journey', 3
+    ),
+    (
+        'Discovering the Wonders of Astrophysics', 2
+    ),
+    (
+        'A Guide to Productivity: Tips and Tricks', 4
+    ),
+    (
+        'Travel Diaries: Adventures in South America', 2
+    ),
+    (
+        'Unraveling the Mysteries of Quantum Physics', 1
+    ),
+    (
+        'Healthy Living: Fitness and Nutrition', 3
+    ),
+    (
+        'A Writer''s Reflection: Finding Inspiration', 2
+    ),
+    (
+        'The Power of Meditation: A Path to Inner Peace', 4
+    ),
+    (
+        'Capturing Moments: Photography as a Passion', 3
+    );
+
+SELECT * from post;
