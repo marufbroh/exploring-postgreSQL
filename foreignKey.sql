6eb0e88a507c832542517418dd45c5bb6dab33d8
Tamim Iqbal Maruf <marufmd775@gmail.com>
2024-03-18 23:34:21 +0600

    Constructing Relationships With Foreign Key Constraints

--- foreignKey.sql
+++ foreignKey.sql
@@ -3,4 +3,10 @@
 CREATE TABLE "user" (
     id SERIAL PRIMARY KEY,
     username VARCHAR(25) not NULL
+);
+
+CREATE TABLE post (
+    id SERIAL PRIMARY KEY,
+    title TEXT NOT NULL,
+    user_id INTEGER REFERENCES "user"(id)
 )
