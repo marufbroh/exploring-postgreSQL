dadb2d326b4f3841b071db1e8f4342ceef146373
Tamim Iqbal Maruf <marufmd775@gmail.com>
2024-03-21 23:21:47 +0600

    Exploring Full, Cross, And Natural Joins

--- cross.sql
+++ cross.sql
@@ -1,24 +1,25 @@
 -- Active: 1709621644738@@127.0.0.1@5432@ph
 
 CREATE TABLE employees (
-    emp_id INT,
-    emp_name VARCHAR(50),
-    dept_id INT
+    emp_id INT, emp_name VARCHAR(50), dept_id INT
 );
 
-CREATE TABLE departments (
-    dept_id INT,
-    dept_name VARCHAR(50)
-);
+CREATE TABLE departments ( dept_id INT, dept_name VARCHAR(50) );
 
-INSERT INTO employees VALUES(1, 'John Doe', 101);
+INSERT INTO employees VALUES (1, 'John Doe', 101);
 
-INSERT INTO employees VALUES(2, 'Jane Smith', 102);
+INSERT INTO employees VALUES (2, 'Jane Smith', 102);
 
 SELECT * from employees;
 
-INSERT INTO departments VALUES( 101, 'Human Recourses');
-INSERT INTO departments VALUES( 102, 'Marketing');
+INSERT INTO departments VALUES (101, 'Human Recourses');
+
+INSERT INTO departments VALUES (102, 'Marketing');
 
 SELECT * from departments;
 
+-- cross join
+SELECT * from employees CROSS JOIN departments;
+
+--  natural join
+SELECT * from employees NATURAL JOIN departments;
