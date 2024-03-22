785c0da2b9931dba9a99c7a2e32d23f6e45ce8c6
Tamim Iqbal Maruf <marufmd775@gmail.com>
2024-03-22 12:29:17 +0600

    Query Practice: Part 2 done

--- practice.sql
+++ practice.sql
@@ -59,31 +59,70 @@ VALUES (
         'Emma Lopez', 10, 60000.00, '2021-01-07'
     );
 
-
-    INSERT INTO employees (employee_name, department_id, salary, hire_date)
-VALUES
-    ('Liam Wilson', 3, 62000.00, '2019-08-25'),
-    ('Ella Thompson', 5, 73000.00, '2017-11-14'),
-    ('Noah Martinez', 4, 78000.00, '2020-03-08'),
-    ('Ava Robinson', 2, 56000.00, '2018-06-20'),
-    ('James Harris', 1, 67000.00, '2016-09-03'),
-    ('Isabella Clark', 6, 71000.00, '2019-12-18'),
-    ('Liam Walker', 8, 74000.00, '2017-05-22'),
-    ('Mia Perez', 7, 69000.00, '2018-10-09'),
-    ('Benjamin Turner', 9, 68000.00, '2020-01-30'),
-    ('Charlotte Baker', 10, 61000.00, '2017-04-12'),
-    ('Alexander Hall', 3, 63000.00, '2018-02-05'),
-    ('Amelia Young', 5, 76000.00, '2020-07-17'),
-    ('Oliver White', 4, 79000.00, '2019-10-28'),
-    ('Sophia Martin', 2, 57000.00, '2017-12-01'),
-    ('William Scott', 1, 68000.00, '2018-04-16'),
-    ('Ava Diaz', 6, 72000.00, '2021-02-22'),
-    ('Elijah Rodriguez', 8, 75000.00, '2019-06-09'),
-    ('Emma Evans', 7, 70000.00, '2016-08-13'),
-    ('Michael Green', 9, 69000.00, '2018-11-05'),
-    ('Olivia Adams', 10, 62000.00, '2020-09-24');
-
-
+INSERT INTO
+    employees (
+        employee_name, department_id, salary, hire_date
+    )
+VALUES (
+        'Liam Wilson', 3, 62000.00, '2019-08-25'
+    ),
+    (
+        'Ella Thompson', 5, 73000.00, '2017-11-14'
+    ),
+    (
+        'Noah Martinez', 4, 78000.00, '2020-03-08'
+    ),
+    (
+        'Ava Robinson', 2, 56000.00, '2018-06-20'
+    ),
+    (
+        'James Harris', 1, 67000.00, '2016-09-03'
+    ),
+    (
+        'Isabella Clark', 6, 71000.00, '2019-12-18'
+    ),
+    (
+        'Liam Walker', 8, 74000.00, '2017-05-22'
+    ),
+    (
+        'Mia Perez', 7, 69000.00, '2018-10-09'
+    ),
+    (
+        'Benjamin Turner', 9, 68000.00, '2020-01-30'
+    ),
+    (
+        'Charlotte Baker', 10, 61000.00, '2017-04-12'
+    ),
+    (
+        'Alexander Hall', 3, 63000.00, '2018-02-05'
+    ),
+    (
+        'Amelia Young', 5, 76000.00, '2020-07-17'
+    ),
+    (
+        'Oliver White', 4, 79000.00, '2019-10-28'
+    ),
+    (
+        'Sophia Martin', 2, 57000.00, '2017-12-01'
+    ),
+    (
+        'William Scott', 1, 68000.00, '2018-04-16'
+    ),
+    (
+        'Ava Diaz', 6, 72000.00, '2021-02-22'
+    ),
+    (
+        'Elijah Rodriguez', 8, 75000.00, '2019-06-09'
+    ),
+    (
+        'Emma Evans', 7, 70000.00, '2016-08-13'
+    ),
+    (
+        'Michael Green', 9, 69000.00, '2018-11-05'
+    ),
+    (
+        'Olivia Adams', 10, 62000.00, '2020-09-24'
+    );
 
 SELECT * from departments;
 
@@ -95,6 +134,34 @@ from employees
 
 SELECT * from employees JOIN departments USING (department_id);
 
-SELECT department_name, round(avg(salary)) as average_salary from employees JOIN departments USING (department_id)
-GROUP BY department_name
-;
+SELECT
+    department_name,
+    round(avg(salary)) as average_salary
+from employees
+    JOIN departments USING (department_id)
+GROUP BY
+    department_name;
+
+SELECT department_name, count(*)
+from employees
+    JOIN departments USING (department_id)
+GROUP BY
+    department_name;
+
+SELECT
+    department_name,
+    avg(salary) as average_salary
+from employees
+    JOIN departments USING (department_id)
+GROUP BY
+    department_name
+ORDER BY average_salary DESC
+LIMIT 1;
+
+SELECT extract(
+        year
+        from hire_date
+    ) as "year", count(*)
+from employees
+GROUP BY
+    "year"
