f172308c3d9342213aed84c605188a95a445e91b
Tamim Iqbal Maruf <marufmd775@gmail.com>
2024-03-24 05:54:51 +0600

    Stored Procedure In PostgreSQL

--- functionsProcedures.sql
+++ functionsProcedures.sql
@@ -19,5 +19,46 @@ $$
 	DELETE from employees WHERE employee_id = p_emp_id;
 $$; 
 
+SELECT delete_employee_by_id (30);
 
-SELECT delete_employee_by_id(30)
+CREATE Procedure remove_emp_var() 
+LANGUAGE plpgsql AS 
+$$
+DECLARE
+	test_var INT;
+	BEGIN
+	SELECT employee_id INTO test_var
+	from employees
+	WHERE
+	    employee_id = 28;
+	DELETE from employees
+	WHERE
+	    employee_id = test_var;
+END
+$$; 
+
+CALL remove_emp_var ();
+
+
+
+CREATE Procedure remove_emp_by_id(p_emp_id INT) 
+LANGUAGE plpgsql AS 
+$$
+DECLARE
+	test_var INT;
+	BEGIN
+	SELECT employee_id INTO test_var
+	from employees
+	WHERE
+	    employee_id = p_emp_id;
+	DELETE from employees
+	WHERE
+	    employee_id = test_var;
+
+
+        RAISE NOTICE 'Employee Removed Successfully!';
+END
+$$; 
+
+
+CALL remove_emp_by_id(27);
